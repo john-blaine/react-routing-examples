587b8762575e5218b7c86cb9008d4c537072375f
John Blaine <jablaine@uw.edu>
2018-06-23 14:32:54 -0600

    Import/Add Header component, and make it global to all routes

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,12 @@ import {
 } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Header from './Header';
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
+      <Header />
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
     </div>
