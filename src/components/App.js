36c2eb8076dffbe8d7c729b1e6592ac44ef13fa9
John Blaine <jablaine@uw.edu>
2018-06-23 14:30:40 -0600

    Add exact prop to make home render only when route is "/"

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import About from './About';
 const App = () => (
   <BrowserRouter>
     <div className="container">
-      <Route path="/" component={Home} />
+      <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
     </div>
   </BrowserRouter>
