daea61a5b7ba39f07c8a3bddd84aa3d4c9ea70b5
John Blaine <jablaine@uw.edu>
2018-06-25 07:47:11 -0600

    Render 404 page if path not matched

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import {
   BrowserRouter,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Courses from './Courses';
 import Header from './Header';
 import Teachers from './Teachers';
+import NotFound from './NotFound';
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
       <Header />
-      <Route exact path="/" component={Home} />
-      <Route path="/about" render={() => <About title='About' /> } />
-      <Route path="/courses" component={Courses} />
-      <Route path="/teachers" component={Teachers} />
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" render={() => <About title='About' /> } />
+        <Route path="/courses" component={Courses} />
+        <Route path="/teachers" component={Teachers} />
+        <Route component={NotFound} />
+      </Switch>
+
     </div>
   </BrowserRouter>
 );
